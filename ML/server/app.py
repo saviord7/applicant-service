0c088be1e1db3a9a0ac8c044164fcd067249ea85
D I M A <savior.7@yandex.ru>
2021-04-09 03:00:24 +0300

    update server

--- ML/server/app.py
+++ ML/server/app.py
@@ -16,27 +16,10 @@ CORS(app)
 def predict():
     json = request.get_json(force=True)
     data_raw = json['data']
-    data=[]
-    
-    for i in data_processing.arr_field_names:
-        if not i in data_raw:
-          data_raw[i] = 0
-          
-        print(i)
-
-        t = 0
-        try:
-          t = int(data_raw[i])
-        except ValueError:
-          
-          
-        data.append(t)
-        
-    print(data)
-    
-    prediction = np.array(data_processing.predict_list(data))
+    prediction = np.array(data_processing.predict_list(data_raw))
     response = make_response('{"prediction": ' + dumps(prediction.tolist()) + '}')
     response.headers['Content-Type'] = "application/json"
+    print(response.data)
     return response
 
 
--- ML/server/data_processing.py
+++ ML/server/data_processing.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 import joblib
+import random
 from sklearn.preprocessing import StandardScaler
 
 arr_field_names = [
@@ -16,22 +17,28 @@ arr_field_names = [
  'НАПРАВЛЕНИЕ_В_ПРИКАЗЕ_ОЦЕНКА_2',
  'НАПРАВЛЕНИЕ_В_ПРИКАЗЕ_ОЦЕНКА_3'
 ]
+data_row = {'УРОВЕНЬ_ДИПЛОМА': 'Среднее', 'БАЛЛ_ЗА_ДОСТИЖЕНИЯ': '5','ЕСТЬ_АТТЕСТАТ_С_ОТЛИЧИЕМ': 'on',
+            'ЕСТЬ_ДИПЛОМ_С_ОТЛИЧИЕМ': 'on',
+            'БАЛЛ_ОЛИМПИАДЫ_И_КОНКУРСЫ': '5', 'ОЦЕНКА_ЗА_СОЧИНЕНИЕ': '10',
+            'НАПРАВЛЕНИЕ_В_ПРИКАЗЕ_ПРЕДМЕТ_1_ПРИОРИТЕТ': 'Английский язык',
+            'НАПРАВЛЕНИЕ_В_ПРИКАЗЕ_ОЦЕНКА_1': '55',
+            'НАПРАВЛЕНИЕ_В_ПРИКАЗЕ_ПРЕДМЕТ_2_ПРИОРИТЕТ': 'Английский язык',
+            'НАПРАВЛЕНИЕ_В_ПРИКАЗЕ_ОЦЕНКА_2': '75',
+            'НАПРАВЛЕНИЕ_В_ПРИКАЗЕ_ПРЕДМЕТ_3_ПРИОРИТЕТ': 'Английский язык',
+            'НАПРАВЛЕНИЕ_В_ПРИКАЗЕ_ОЦЕНКА_3': '90', 'НАПРАВЛЕНИЕ_В_ПРИКАЗЕ_БАЛЛ': '210',
+            'ОБЩЕЖИТИЕ_ТИП_ЗАСЕЛЕНИЯ': 'Не требуется'}
 
-data = np.array([[0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0],
-                 [10, 10, 10, 0, 0, 2, 0, 230, 70, 60, 100],
-                 [0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0]])
 
-
-def preprocessing(data=data):
+def normalize(data):
     sc = StandardScaler()
     data = sc.fit_transform(data)
     return data
 
 
-def predict_list(data=data, models_dir='../models/LR_models/'):
+def predict_list(data, models_dir='../models/LR_models/'):
     #print(data)
     data = preprocessing(data)
+    data = normalize(data)
     prediction_list = []
     all_models = os.listdir(models_dir)
     for item in all_models:
@@ -41,5 +48,19 @@ def predict_list(data=data, models_dir='../models/LR_models/'):
         #print(prediction)
         prediction = np.append(prediction, item[:8])
         prediction_list.append(prediction)
-
+    prediction_list.sort(key=lambda x: x[0], reverse=True)
     return prediction_list
+
+
+
+def preprocessing(data_row):
+    data = []
+    for i in arr_field_names:
+        try:
+           example = int(data_row[i])
+        except:
+           example = random.randint(0,1)
+  #      print(data_row[i])
+        data.append(example)
+    data = np.array([data])
+    return data
