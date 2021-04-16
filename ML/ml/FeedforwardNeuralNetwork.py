c1b0e76a02ebc0c6b9e3e0dac9404d19fdfa4a20
D I M A <savior.7@yandex.ru>
2021-04-17 02:56:33 +0300

    add FNN, fix some bugs

---
+++ ML/ml/FeedforwardNeuralNetwork.py
@@ -0,0 +1,73 @@
+import numpy as np
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras import optimizers
+
+from sklearn.preprocessing import Normalizer
+from sklearn.model_selection import train_test_split
+import data_loading_and_preprocessing
+
+
+
+# Load data prepared data
+data = data_loading_and_preprocessing.make_prepared_data()
+
+# Find unique courses:
+course = data['НАПРАВЛЕНИЕ_ПОДГОТОВКИ'].unique()
+
+for example in course:
+    x = data.loc[data['НАПРАВЛЕНИЕ_ПОДГОТОВКИ'] == example]
+    x = x.drop(['НАПРАВЛЕНИЕ_ПОДГОТОВКИ', 'НАПРАВЛЕНИЕ_В_ПРИКАЗЕ_БАЛЛ_ЕГЭ_С_ОЛИМПИАДОЙ',
+                'НАПРАВЛЕНИЕ_В_ПРИКАЗЕ_ОЛИМПИАДА_ЗА_100_БАЛЛОВ', 'keyID'], axis=1)
+
+    print('#################################')
+    print('INFO:')
+    print('Training ', example, ' course..')
+    print('#################################')
+
+    print('Dividing data...')
+    train_data = np.array(x.drop('avg_mark', axis=1))
+    train_label = x['avg_mark']
+
+    norm = Normalizer()
+
+    print('Preprocessing data...')
+    train_data = norm.fit_transform(train_data)
+
+    train_X, test_X, train_y, test_y = train_test_split(train_data, train_label, test_size=0.1, random_state=1)
+
+    print(train_X.shape)
+    train_y = np.array(train_y)
+    test_y = np.array(test_y)
+
+    #Network
+    model = Sequential()
+    model.add(Dense(22, activation='relu', input_shape=(train_X.shape[1],)))
+    model.add(Dropout(0.2))
+    model.add(Dense(11, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(22, activation='relu'))
+    model.add(Dropout(0.2))
+
+    model.add(Dense(11, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(1))
+
+    opt = optimizers.SGD(learning_rate=0.01)
+    #Compile network
+    model.compile(optimizer=opt, loss='mse', metrics=['mae'])
+
+    print('Fitting model...')
+    history = model.fit(train_X, train_y, epochs=7, batch_size=1)
+
+    mse, mae = model.evaluate(test_X, test_y)
+    print('MSE on test data : ', mse)
+    print('MAE on test data : ', mae)
+
+    print('Saving model ...')
+    filename = '../models/FNN_models/' + example + '-model'
+    model.save(filename)
+    print('END INFO')
+    print('#################################')
+    print()
--- ML/ml/LinearRegression.py
+++ ML/ml/LinearRegression.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import Normalizer
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
@@ -28,21 +28,16 @@ for example in course:
     print('#################################')
 
     print('Dividing data...')
+    model = LinearRegression(normalize=True)
     train_data = np.array(x.drop('avg_mark', axis=1))
     train_label = x['avg_mark']
 
-    sc = StandardScaler()
-
-    print('Preprocessing data...')
-    train_data = sc.fit_transform(train_data)
-
-    print('Splitting data for test / train ...')
-    train_X, test_X, train_y, test_y = train_test_split(train_data, train_label, test_size=0.1, random_state=1)
+    print('Spliting data for test / train ...')
+    train_X, test_X, train_y, test_y = train_test_split(train_data, train_label, test_size=0.05, random_state=1)
 
     train_y = np.array(train_y)
     test_y = np.array(test_y)
 
-    model = LinearRegression()
 
     print('Fitting model...')
     model.fit(train_X, train_y)
